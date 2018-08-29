0a57a133fc8e23dd3ac75763d1b9bd6e9b2a775c
Matt Levin <levinmat@gmail.com>
2018-08-29 17:31:34 -0400

    Generic commit message

--- static/js/main.js
+++ static/js/main.js
@@ -205,7 +205,7 @@ $(document).ready(function() {
 	// On mobile, select all text in search field when clicked
 	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
 		alert("mobile");
-		$('#searchField').onclick( function(e) {
+		$('#searchField').click( function(e) {
 			alert("clicked!");
 			setTimeout(function() {
 				try {
